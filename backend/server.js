630c6daaaf2ad219c81117820c216615c1e6ecb1
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-16 13:36:31 +0100

    I created the backed folder for the api, a server.js file, i then added package.json file using the npm init. I set up the app to listen on port 4000 in the server.js file. I began tracking changes to the gratitude app using git init, making the .gitignore file and ingnoring the /node_modules.

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// express app
+const app = express();
+
+app.listen('4000', () => console.log('App listening on port 4000'));
