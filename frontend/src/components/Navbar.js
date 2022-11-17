2678f2000fbfee8d360e3e76f757fe6d6007d855
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-17 14:45:42 +0100

    I added a /assets folder and added my logo.png. I imported it in the navbar.js component and added an img tag. I then added the first codes of the navbar in the index.css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">
-        <h1>GratitudeApp</h1>
-      </Link>
-      <ul>
-        <li>
-          <Link to="/signin">Sign in</Link>
-        </li>
-        <li>
-          <Link to="/login">Log in</Link>
-        </li>
-        <li>
-          <Link to="/logout">Log out</Link>
-        </li>
-      </ul>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="gratitude-journal-logo" />
+        </Link>
+      </div>
+
+      <div>
+        <ul>
+          <li>
+            <Link to="/signin">Sign in</Link>
+          </li>
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+          <li>
+            <Link to="/logout">Log out</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
