e0388616932678619bf442285424d070c85e4b27
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-17 10:51:49 +0100

    I created the gratitude controller functions to enable me create, delete, and update gratitude items, I tested the in postman and then made the commit

--- backend/controllers/gratitudeController.js
+++ backend/controllers/gratitudeController.js
@@ -1,21 +1,39 @@
 const Gratitude = require('../models/gratitudeModel');
+const mongoose = require('mongoose');
 
 // get all the gratitude items listed out
-const getGratitudes = (req, res) => {
-  res.json({ mssg: 'Get all gratitude items' });
+const getGratitudes = async (req, res) => {
+  try {
+    const gratitude = await Gratitude.find({}).sort({ createdAt: -1 });
+    res.status(200).json(gratitude);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 };
 
 // get a single gratitude item listed out
-const getGratitude = (req, res) => {
-  res.json({ mssg: 'Get a single gratitude item' });
+const getGratitude = async (req, res) => {
+  // res.json({ mssg: 'Get a single gratitude item' });
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such gratitude id' });
+  }
+
+  const gratitude = await Gratitude.findById(id);
+  if (!gratitude) {
+    return res.status(404).json({ error: 'No such gratitude item' });
+  } else {
+    res.status(200).json(gratitude);
+  }
 };
 
 // Create a new gratitude item
 const createGratitude = async (req, res) => {
-  const content = req.body;
+  const { content } = req.body;
 
   try {
-    const gratitude = await Gratitude.create(content);
+    const gratitude = await Gratitude.create({ content });
     res.status(200).json(gratitude);
   } catch (error) {
     res.json({ error: error.message });
@@ -23,13 +41,37 @@ const createGratitude = async (req, res) => {
 };
 
 // delete a gratitude item
-const deleteGratitude = (req, res) => {
-  res.json({ mssg: 'Delete a gratitude item' });
+const deleteGratitude = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such gratitude id' });
+  }
+
+  const gratitude = await Gratitude.findOneAndDelete({ _id: id });
+  if (!gratitude) {
+    return res.json({ mssg: 'No such gratitude item to delete' });
+  } else {
+    res.status(200).json(gratitude);
+  }
 };
 
 // update a gratitude item
-const updateGratitude = (req, res) => {
-  res.json({ mssg: 'Update gratitude item' });
+const updateGratitude = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'No such item to update' });
+  }
+  const gratitude = await Gratitude.findOneAndUpdate(
+    { _id: id },
+    { ...req.body }
+  );
+  if (!gratitude) {
+    return res.json({ mssg: 'Update gratitude item' });
+  } else {
+    res.status(200).json(gratitude);
+  }
 };
 
 module.exports = {
