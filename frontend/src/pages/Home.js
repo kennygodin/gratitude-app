88e311452e4f54c178d5165b8c280fcb2d0d6ed7
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-18 17:05:06 +0100

    I added styles for the homepage in the index.css for the navbar, container, body, form, inputs buttons, ul, li, and footer elements. This is the initial style. I also made some adjustments in the Home.js component. I made the commit thereafter.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome.</h1>
+      <div className="container">
+        <form>
+          <input type="text" placeholder="What are you grateful for today?" />
+          <button>Add</button>
+        </form>
+        <ul>
+          <li>
+            I am grateful for health<div className="del">DELETE</div>
+          </li>
+          <li>
+            I am grateful for family<div className="del">DELETE</div>
+          </li>
+          <li>
+            I am grateful for friends<div className="del">DELETE</div>
+          </li>
+          <li>
+            I am gratful for provision<div className="del">DELETE</div>
+          </li>
+          <li>
+            I am grateful for mentors<div className="del">DELETE</div>
+          </li>
+        </ul>
+
+        <div className="footer">
+          <p>
+            <strong>I'm grateful for 5 things today!</strong>
+          </p>
+          <button>DELETE ALL</button>
+        </div>
+      </div>
     </div>
   );
 };
