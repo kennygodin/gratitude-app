1ff96a058a8218c21c69b26561b323cdb2815599
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-19 01:11:10 +0100

    I installed fontawsome dependancy for react, I changed entirely the navbar code in the css and made adjustments in the Navbar.js component and made the commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle('responsive_nav');
+  };
   return (
     <div className="navbar">
-      <div>
+      <div className="header">
         <Link to="/">
           <img src={logo} alt="gratitude-journal-logo" />
         </Link>
       </div>
-      <Link to="#" className="toggle-btn">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </Link>
-      <div>
+
+      <div ref={navRef} className="nav">
         <ul>
           <li>
             <Link to="/signin">Sign in</Link>
@@ -25,8 +28,14 @@ const Navbar = () => {
           <li>
             <Link to="/logout">Log out</Link>
           </li>
+          <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+            <FaTimes />
+          </button>
         </ul>
       </div>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
     </div>
   );
 };
