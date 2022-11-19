0f03a5c5d4357e02cf094a9a3347d6c812bef404
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-19 03:34:59 +0100

    I created a state [content, setContent] to store the values of the input. I added the value and onChange attributes in the input. I added a onSubmit handler called handleSubmit and then using the post method i reached out to the correct endpoint for a post request. I checked for errors and the reset the states of both content and errors. I also conditionally rendered the error only if there's one.

--- frontend/src/components/GratitudeForm.js
+++ frontend/src/components/GratitudeForm.js
@@ -1,12 +1,45 @@
+import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 const GratitudeForm = () => {
+  const [content, setContent] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const gratitude = { content };
+
+    const response = await fetch('/api/gratitudes', {
+      method: 'POST',
+      body: JSON.stringify(gratitude),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      setError(null);
+      setContent('');
+      console.log('New gratitude item added', json);
+    }
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="What are you grateful for today?" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="What are you grateful for today?"
+      />
       <button>
         <FaPlus />
       </button>
+      {/* {error && <div>{error}</div>} */}
     </form>
   );
 };
