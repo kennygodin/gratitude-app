11d93336839379e701c9acf3fb9ea181a460dfae
kennycodin <kenechukwuokoh30@gmail.com>
2022-11-19 09:51:48 +0100

    I created a state using the useReducer [state, dispatch], having it inital state of gratitudes to be null. I added a gratitude reducer function where all the logic for the state change will be added. In the function, passing (state, action) as parameters, I used a switch statement of (action.type) and set the cases of SET_GRATTITUDES, CREATE_GRATITUDE.

--- frontend/src/context/GratitudeContext.js
+++ frontend/src/context/GratitudeContext.js
@@ -1,7 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const GratitudesContext = createContext();
 
+export const gratitudeReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_GRATITUDES':
+      return {
+        gratitudes: action.payload,
+      };
+    case 'CREATE_GRATITUDE':
+      return {
+        gratitudes: [action.payload, ...state.gratitudes],
+      };
+    default:
+      return state;
+  }
+};
 export const GratitudesContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(gratitudeReducer, {
+    gratitudes: null,
+  });
+
   return <GratitudesContext.Provider>{children}</GratitudesContext.Provider>;
 };
